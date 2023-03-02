b4c005641cf6d01c6480bfc4bdaf81c4c81469b9
Timothy6Kyalo <tkyalo81@gmail.com>
2023-03-02 12:16:21 +0300

    initial commit

--- djangocrud/views.py
+++ djangocrud/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from django.contrib.
+from django.contrib.auth import authenticate, login, logout
+from .models import Student
 
 
 def Home(request):
