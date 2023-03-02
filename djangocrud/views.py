8ed5ee9e672f3456301bb4c73e4143af6936642c
Timothy6Kyalo <tkyalo81@gmail.com>
2023-03-02 11:23:29 +0300

    Initial Commit

---
+++ djangocrud/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.
+
+
+def Home(request):
+    return render(request, 'index.html')
+
+
+def handlesignup(request):
+    return render(request, 'signup.html')
+
+def handlelogin(request):
+    return render(request, 'login.html')
+def updateData(request):
+    return render(request,'edit.html')
+def insertData(request):
+    if request.method=="POST":
+        name=request.POST.get("name")
+        email=request.POST.get("email")
+        age=request.POST.get("age")
+        gender=request.POST.get("gender")
+        query=Student(name=name, email=email,age=age,gender=gender)
+        query.save()
+        return redirect('my-index')
+    return render(request, 'index.html')
+def deleteData(request):
+    return render(request, 'index.html')
+def handlelogout(request):
+    return render(request, 'my-signup')
